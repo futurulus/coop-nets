4f86503570f1b7d13ed74733523f9bb32fbe150a
Will Monroe <wmonroe4@stanford.edu>
2016-06-15 00:13:19 -0500

    Add kwargs (e.g. deterministic) to ForgetSizeLayer

--- helpers.py
+++ helpers.py
@@ -26,10 +26,10 @@ class ForgetSizeLayer(Layer):
         super(ForgetSizeLayer, self).__init__(incoming, **kwargs)
         self.axis = axis
 
-    def get_output_for(self, input):
+    def get_output_for(self, input, **kwargs):
         return input
 
-    def get_output_shape_for(self, input_shape):
+    def get_output_shape_for(self, input_shape, **kwargs):
         shape = list(input_shape)
         shape[self.axis] = None
         return tuple(shape)
