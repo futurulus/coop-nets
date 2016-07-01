2ee6b70543d67ba4712e24e9b690fabe5409c526
Will Monroe <wmonroe4@cs.stanford.edu>
2016-07-01 10:42:40 -0700

    Add HTML formatting to RSA data generation

--- rsa_fit_data.py
+++ rsa_fit_data.py
@@ -1,6 +1,8 @@
 import sys
 from collections import Counter
 
+from stanza.research import config
+
 import colordesc
 from color_instances import get_training_instances
 from speaker import UniformContextPrior
@@ -8,21 +10,29 @@ from ref_game import ExhaustiveS1Learner
 from html_report import format_value
 
 
-def generate_dataset(num_insts=100):
+def generate_dataset(num_insts=100, print_html=True):
     cd = colordesc.ColorDescriber()
     get_prior_counter()
     exh = ExhaustiveS1Learner(cd.model)
-    insts = UniformContextPrior().sample(num_insts)
-    context_size = len(insts[0].alt_inputs)
-    print('<html><head><title>RSA data</title></head><body>')
-    print('<table><tr><th>context</th>' + '<th></th>' * (context_size - 1) +
-          '<th>target</th><th>prediction</th></tr>')
-    for inst, pred in zip(insts, exh.predict(insts, random=True)):
-        cols = list(inst.alt_inputs) + [inst.input, pred]
-        print('<tr>')
-        print(''.join(format_value(v) for v in cols))
-        print('</tr>')
-    print('</table></body></html>')
+    contexts = UniformContextPrior().sample(num_insts)
+    context_size = len(contexts[0].alt_inputs)
+    if print_html:
+        print('<html><head><title>RSA data</title></head><body>')
+        print('<table><tr><th>context</th>' + '<th></th>' * (context_size - 1) +
+              '<th>target</th><th>prediction</th></tr>')
+    insts = []
+    for inst, pred in zip(contexts, exh.predict(contexts, random=True)):
+        inst = inst.inverted()
+        inst.input = pred
+        insts.append(inst)
+        cols = list(inst.alt_outputs) + [inst.output, pred]
+        if print_html:
+            print('<tr>')
+            print(''.join(format_value(v) for v in cols))
+            print('</tr>')
+    if print_html:
+        print('</table></body></html>')
+    return insts
 
 
 def get_prior_counter():
@@ -34,4 +44,5 @@ def get_prior_counter():
 
 
 if __name__ == '__main__':
-    generate_dataset()
+    insts = generate_dataset(num_insts=100, print_html=False)
+    config.dump([inst.__dict__ for inst in insts], 'data.out.jsons', lines=True)
