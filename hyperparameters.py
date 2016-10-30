02841ba76a31748da912457bfb66b0fc00f5ec5d
Will Monroe <wmonroe4@stanford.edu>
2016-10-29 18:18:30 -0700

    Add support for true grid search

--- hyperparameters.py
+++ hyperparameters.py
@@ -10,6 +10,7 @@ that loads those settings. The files will be created in the `run_dir`.
 '''
 import os
 import random
+import sys
 
 from stanza.research import config
 
@@ -27,14 +28,22 @@ parser.add_argument('--grid', default=None,
                     help='Path to a tab-separated value file containing parameter names in '
                          'the first column, followed by possible values for that parameter '
                          '(as strings).')
+parser.add_argument('--grid_search', default=None,
+                    help='If not None, a path to a file containing parameter settings to use '
+                         'as default in a grid search (try running all settings with '
+                         'only one parameter different). The parameter settings should be '
+                         'one line of an args file (space-separated values).')
 
 
 def output_tuning_files(options):
     param_names, grid = load_grid(options.grid)
-    with config.open('args', 'w') as argsfile:
-        for i in range(options.num_samples):
-            gpu_num = [str(i % 4)] if options.cycle_gpus else []
-            argsfile.write(' '.join([random.choice(a) for a in grid] + gpu_num) + '\n')
+    if options.grid_search is None:
+        with config.open('args', 'w') as argsfile:
+            for i in range(options.num_samples):
+                gpu_num = [str(i % 4)] if options.cycle_gpus else []
+                argsfile.write(' '.join([random.choice(a) for a in grid] + gpu_num) + '\n')
+    else:
+        generate_grid_search(grid, options)
     with config.open('command', 'w') as commandfile:
         commandfile.write("parallel -j%d --delay 30 --tagstring " % options.num_processes)
         commandfile.write(build_tag_string(param_names))
@@ -60,6 +69,32 @@ def load_grid(filename):
             param_names.append(cols[0])
             grid.append(cols[1:])
     return param_names, grid
+
+
+def generate_grid_search(grid, options):
+    with open(options.grid_search, 'r') as settingsfile:
+        base_settings = list(settingsfile)[0].split()
+    search_configs = []
+    gpu_num = 0
+    for i, (base, values) in enumerate(zip(base_settings, grid)):
+        try:
+            base_index = values.index(base)
+        except ValueError:
+            print >>sys.stderr, 'Base setting "%s" not found in options %s' % (base, values)
+        for j in range(len(values)):
+            if values[j] != base:
+                dist = abs(j - base_index)
+                new_config = base_settings[:i] + [values[j]] + base_settings[i + 1:]
+                if options.cycle_gpus:
+                    new_config.append(str(gpu_num))
+                    gpu_num = (gpu_num + 1) % options.cycle_gpus
+                search_configs.append((dist, new_config))
+
+    search_configs.sort(key=lambda p: p[0])
+
+    with config.open('args', 'w') as argsfile:
+        for dist_, new_config in search_configs:
+            argsfile.write(' '.join(new_config) + '\n')
 
 
 def build_tag_string(param_names):
