6506635a1f1014c2daac5974e2e37f0f2575a520
Andrew Jong <andrewjong87@gmail.com>
2017-10-23 22:39:29 -0700

    Added function to filter out spam games containing 25 or more of the same message per game

--- languageAnalysis/clean_message_data.py
+++ languageAnalysis/clean_message_data.py
@@ -2,7 +2,7 @@
 """
 Clean up the data
 1 - Removes messages greater than 4 standard deviations away from the mean (do this first)
-2 - Remove the spam messages :c
+2 - Remove the spam messages :c (TOOD)
 Author: Andrew
 """
 import pandas as pd
@@ -15,37 +15,57 @@ MESSAGE_COLUMN = 'contents'
 SEPARATOR = ','
 NUM_COLUMNS = 5
 MAX_STD = 4
+MAX_DUPLICATES = 25
 
 def main():
     """
-    Performs preprocessing (cleanup) on the chinese 
+    Performs preprocessing (cleanup) on the chinese message data
     """
     # remove extra commas from the messages
     stripped_file_name = remove_extra_separators(IN_DIR + INPUT_FILE)
     opened_stripped_f = open(stripped_file_name, 'r')
 
-    # read into a pandas df
+    # read csv into a pandas df
     raw_df = pd.read_csv(opened_stripped_f, sep=SEPARATOR)
+    print 'Columns: ', raw_df.columns
     raw_num_rows = raw_df.shape[0]
     print 'Original num datapoints: ', raw_num_rows
 
     # filter out the long messages and get information
-    filtered_length_df, removed_df, mean, std = filter_long_messages(raw_df, MAX_STD)
-    filtered_num_rows = filtered_length_df.shape[0]
-
-    # print summary results
-    print 'Filtered by length num datapoints: ', filtered_num_rows
-    print 'Difference: ', raw_num_rows - filtered_num_rows
+    print 'Filtering out messages with lengths exceeding {} standard deviations.'.format(MAX_STD)
+    filtered_len_df, deleted_len_df, mean, std = filter_long_messages(raw_df, MAX_STD)
+    fil_len_num_rows = filtered_len_df.shape[0]
+    # print summary results for filtered by length
     print 'Mean: {}, std: {}'.format(mean, std)
+    print 'Num messages removed by excessive length:', raw_num_rows - fil_len_num_rows
+    print 'Num datapoints remaining:', fil_len_num_rows
 
-    # write to file
+    # filter out the spam games
+    print '\nFiltering out messages with spams containing more than {} duplicates.'.format(MAX_DUPLICATES)
+    filtered_spam_df, deleted_spam_df = filter_spam_games(filtered_len_df, MAX_DUPLICATES)
+    num_spam_games = len(deleted_spam_df.groupby('gameid'))
+    fil_spam_num_rows = filtered_spam_df.shape[0]
+    # print summary results for filtered by spam
+    print 'Num spam games: ', num_spam_games
+    print 'Num messages removed by spam:', fil_len_num_rows - fil_spam_num_rows
+    print 'Num datapoints remaining:', fil_spam_num_rows
+
+    # Write filtered to file
+    print 'Writing to file'
     filtered_out_name = OUT_DIR + INPUT_FILE[:-4] + '_filtered.csv'
     print 'Writing filtered to ', filtered_out_name
-    filtered_length_df.to_csv(filtered_out_name, encoding='utf-8')
-    
-    removed_out_name = OUT_DIR + 'filtered_removed_messages.csv'
-    print 'Writing removed messages to ', removed_out_name
-    removed_df.to_csv(removed_out_name, encoding='utf-8')
+    filtered_spam_df.to_csv(filtered_out_name, encoding='utf-8')
+
+    # Write what got removed, to check what got removed makes sense
+    del_len_out_name = OUT_DIR + 'filtered_deleted_long_lengths.csv'
+    print 'Writing removed messages to ', del_len_out_name
+    deleted_len_df.to_csv(del_len_out_name, encoding='utf-8')
+
+    del_spam_out_name = OUT_DIR + 'filtered_deleted_spam.csv'
+    print 'Writing removed messages to ', del_spam_out_name
+    deleted_spam_df.to_csv(del_spam_out_name, encoding='utf-8')
+
+    print 'Done.'
 
 
 def remove_extra_separators(input_file):
@@ -91,16 +111,30 @@ def filter_long_messages(raw_df, std=4):
     raw_df['z_scores'] = (raw_df['raw_lengths'] - length_mean) / length_std
     # keep based on z_score (the num of standard deviations)
     filtered_length_df = raw_df[raw_df['z_scores'] <= std]
-    removed_rows_df = raw_df[raw_df['z_scores'] > std]
+    deleted_length_df = raw_df[raw_df['z_scores'] > std]
 
     # remove the columns we added for calculating and filtering
     filtered_length_df = filtered_length_df.drop('raw_lengths', axis=1)
     filtered_length_df = filtered_length_df.drop('z_scores', axis=1)
-    removed_rows_df = removed_rows_df.drop('raw_lengths', axis=1)
-    removed_rows_df = removed_rows_df.drop('z_scores', axis=1)
 
-    return filtered_length_df, removed_rows_df, length_mean, length_std
+    return filtered_length_df, deleted_length_df, length_mean, length_std
 
+def filter_spam_games(df, threshold):
+    """
+    Filter out games (based on gameid) with duplicated messages greater than or equal to the threshold value. 
+    A message is considered to be a duplicate if it appears more than once.
+    E.g. if the threshold is 5, games with 5 or more duplicated messages
+
+    :param df: the message df to filter
+    :param threshold: the max number (exclusive) of duplicate messages. The max duplicated message is compared to the 
+        threshold, ie 3 occurences of 'aaa' and 2 occurences of 'bbb' has a max duplicated value of 3, thus 3 is used.
+    :returns: tuple first containing the filtered dataframe, and second the rows that were deleted
+    """
+    # filter out bad games. group by id, then group by the contents, then get the max size of the grouped contents
+    filtered_spam_df = df.groupby('gameid').filter(lambda g: g.groupby('contents').size().max() < threshold)
+    deleted_spam_df = df.groupby('gameid').filter(lambda g: g.groupby('contents').size().max() >= threshold)
+
+    return filtered_spam_df, deleted_spam_df
 
 
 if __name__ == '__main__':
