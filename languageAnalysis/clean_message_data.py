92b95d22b47735bd7d4d59219b6ab31174472704
Andrew Jong <andrewjong87@gmail.com>
2017-10-26 19:02:59 -0700

    Added function to filter for self-reported confused players

--- languageAnalysis/clean_message_data.py
+++ languageAnalysis/clean_message_data.py
@@ -6,14 +6,16 @@ Cleans up the data from the chinese message corpus
 2 - Removes spam games
 Author: Andrew
 """
+import os
 import pandas as pd
+import numpy as np
 import re
 import jieba
 import nltk.tokenize
 
 # === Constants ===
-IN_DIR = 'data_input_raw/'
-OUT_DIR = 'data_input_cleaned/'
+IN_DIR = 'data_input_raw'
+OUT_DIR = 'data_input_cleaned'
 INPUT_FILE = 'colorReferenceMessageChinese.csv'
 MESSAGE_COLUMN = 'contents'
 SEPARATOR = ','
@@ -27,20 +29,15 @@ def main():
     """
     Performs preprocessing (cleanup) on the chinese message data
     """
-    # remove extra commas from the messages
-    stripped_file_name = remove_extra_separators(IN_DIR + INPUT_FILE)
-    opened_stripped_f = open(stripped_file_name, 'r')
-
-    # read csv into a pandas df
-    raw_df = pd.read_csv(opened_stripped_f, sep=SEPARATOR)
+    raw_df = open_raw_df()
     print 'Columns:', raw_df.columns
     raw_num_rows = raw_df.shape[0]
     print 'Original num datapoints:', raw_num_rows
 
     # filter out the long messages and get information
     print '\nFiltering out messages with word counts exceeding {} standard deviations.'.format(MAX_STD)
-    filtered_len_df, deleted_len_df, mean, std = filter_long_messages(raw_df, MAX_STD)
-    fil_len_num_rows = filtered_len_df.shape[0]
+    filtered_df, deleted_len_df, mean, std = filter_long_messages(raw_df, MAX_STD)
+    fil_len_num_rows = filtered_df.shape[0]
     # print summary results for filtered by length
     print 'Mean: {}, std: {}'.format(mean, std)
     print 'Num messages removed by excessive word counts:', raw_num_rows - fil_len_num_rows
@@ -48,32 +45,53 @@ def main():
 
     # filter out the spam games
     print '\nFiltering out messages with spams containing more than {} duplicates.'.format(MAX_DUPLICATES)
-    filtered_spam_df, deleted_spam_df = filter_spam_games(filtered_len_df, MAX_DUPLICATES)
+    filtered_df, deleted_spam_df = filter_spam_games(filtered_df, MAX_DUPLICATES)
     num_spam_games = len(deleted_spam_df.groupby('gameid'))
-    fil_spam_num_rows = filtered_spam_df.shape[0]
+    fil_spam_num_rows = filtered_df.shape[0]
     # print summary results for filtered by spam
     print 'Num spam games:', num_spam_games
     print 'Num messages removed by spam:', fil_len_num_rows - fil_spam_num_rows
     print 'Num datapoints remaining:', fil_spam_num_rows
     print '\nTotal removed:', raw_num_rows - fil_spam_num_rows
 
+    # filter out the misunderstood games
+    print '\nFiltering out games from players reporting confusion of the game.'
+    filtered_df, deleted_confused_df = filter_confused_players(filtered_df)
+    fil_confused_num_rows = filtered_df.shape[0]
+    # print summary results for filtered by spam
+    print 'Num messages removed from confused:', fil_spam_num_rows - fil_confused_num_rows
+    print 'Num datapoints remaining:', fil_confused_num_rows
+    print '\nTotal removed:', raw_num_rows - fil_confused_num_rows
+
     # Write filtered to file
     print '\nWriting to file'
-    filtered_out_name = OUT_DIR + INPUT_FILE[:-4] + '_filtered.csv'
+    filtered_out_name = os.path.join(OUT_DIR, INPUT_FILE[:-4] + '_filtered.csv')
     print 'Writing filtered to', filtered_out_name
-    filtered_spam_df.to_csv(filtered_out_name, encoding='utf-8', index=False)
+    filtered_df.to_csv(filtered_out_name, encoding='utf-8', index=False)
 
     # Write what got removed, to check what got removed makes sense
-    del_len_out_name = OUT_DIR + 'filtered_deleted_long_lengths.csv'
+    # long
+    del_len_out_name = os.path.join(OUT_DIR, 'filtered_deleted_long_lengths.csv')
     print 'Writing removed messages from excessive word counts to', del_len_out_name
     deleted_len_df.to_csv(del_len_out_name, encoding='utf-8', index=False)
-
-    del_spam_out_name = OUT_DIR + 'filtered_deleted_spam.csv'
+    # spam
+    del_spam_out_name = os.path.join(OUT_DIR, 'filtered_deleted_spam.csv')
     print 'Writing removed messages from spam games to', del_spam_out_name
     deleted_spam_df.to_csv(del_spam_out_name, encoding='utf-8', index=False)
+    # confused
+    del_confused_out_name = os.path.join(OUT_DIR,'filtered_deleted_confused.csv')
+    print 'Writing removed messages from confused players to', del_confused_out_name
+    deleted_confused_df.to_csv(del_confused_out_name, encoding='utf-8', index=False)
 
     print 'Done.'
 
+def open_raw_df():
+    # remove extra commas from the messages
+    stripped_file_name = remove_extra_separators(os.path.join(IN_DIR, INPUT_FILE))
+    opened_stripped_f = open(stripped_file_name, 'r')
+
+    # read csv into a pandas df
+    return pd.read_csv(opened_stripped_f, sep=SEPARATOR)
 
 def remove_extra_separators(input_file):
     """
@@ -160,6 +178,49 @@ def filter_spam_games(df, threshold):
     deleted_spam_df = df.groupby('gameid').filter(lambda g: g.groupby('contents').size().max() >= threshold)
 
     return filtered_spam_df, deleted_spam_df
+
+def filter_confused_players(message_df):
+    """
+    docstring here
+    :param message_df: pandas dataframe containing the messages
+    :param hit_df: pandas dataframe containing the hits
+    """
+    hit_df = hit_results_to_df()
+    # combine together the Answer1-4 columns, because for some reason data is everywhere in there
+    hit_df['feedback'] = hit_df['Answer 1'].map(str) + hit_df['Answer 2'].map(str) + hit_df['Answer 3'].map(str) \
+        + hit_df['Answer 4'].map(str)
+    hit_df['understood'] = hit_df['feedback'].apply(lambda x: '"understood":"yes"' in x)
+    hit_df['gameid'] = hit_df['feedback'].apply(find_game_id)
+    # use only the gameid and understood columns
+    hit_df = hit_df[['gameid', 'understood']]
+    hit_df = hit_df.dropna(subset=['gameid'])
+    # use the non-understanding as a mask
+    hit_df = hit_df[~hit_df.understood]
+
+    filtered_df = message_df[~message_df['gameid'].isin(hit_df['gameid'])].reset_index()
+    deleted_df = message_df[message_df['gameid'].isin(hit_df['gameid'])].reset_index()
+
+    return filtered_df, deleted_df
+
+def hit_results_to_df():
+    """
+    Combines the .csv files in the /hit_results/ directory into a dataframe.
+    :returns: the combined dataframe
+    """
+    df = pd.DataFrame() # init an empty dataframe
+    for filename in os.listdir(os.path.join('hit_results')):
+        if filename.startswith('HITResults') and filename.endswith('.csv'):
+            sub_df = pd.read_csv(os.path.join('hit_results', filename))
+            if len(sub_df.columns) == 13 and 'Answer 4' in sub_df.columns:
+                df = df.append(sub_df, ignore_index=True)
+    return df
+
+def find_game_id(string):
+    match = re.search(r'"+gameID"+:"+([\w-]+)"+', string)
+    if match is not None:
+        return match.group(1)
+    else:
+        return np.nan
 
 
 if __name__ == '__main__':
